226ea7f73de7e2cda5fccaf99ffef351d60709ac
NunoFilipeRosaPires <nunofiliperosapires@gmail.com>
2023-08-03 14:09:01 +0100

    call the page modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
+import { createContext, useState } from "react";
+import { Container, Navbar, RepositoryList, UserList } from "./components";
+
+export const SearchContext = createContext(undefined);
+
 function App() {
-  return <div>App</div>;
+  const [searchValue, setSearchValue] = useState("");
+
+  return (
+    <div className="app">
+      <Navbar value={searchValue} setValue={setSearchValue} />
+      <Container>
+        <>
+          <UserList title="Trending Users" sort="followers" search={searchValue} />
+          <UserList title="Most Active Users" sort="repositories" search={searchValue} />
+          <RepositoryList search={searchValue} />
+        </>
+      </Container>
+    </div>
+  );
 }
 
 export default App;
